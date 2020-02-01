b82bf0b9d41b314258e07f5fb5f02310a5af52fb
Chad Hinds <chadrickj8@gmail.com>
2020-02-01 16:12:11 -0500

    State the use of built in replace method

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function removeUrlAnchor(url){
-    // Use the built in replace method to do stuff
+    // Use the built in replace method to work with string
     return url.replace(/#.*/gi,"");
 }
